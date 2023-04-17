98ac3a48c02221643627ddd44567521e0119c400
Vatsal Patel <vatsal4011@gmail.com>
2023-04-17 11:54:30 +0530

    fixed incorrect api call

---
+++ pages/api/userTransactions/[userEmail].js
@@ -0,0 +1,15 @@
+import prisma from "../../utils/prismaClient";
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const { userEmail } = req.query;
+    const newObject = await prisma.transaction.findMany({
+      where: {
+        userEmail: userEmail,
+      },
+    });
+
+    res.status(200).json(req.body);
+  } else {
+    res.status(400).json({ message: "Method not allowed" });
+  }
+}
