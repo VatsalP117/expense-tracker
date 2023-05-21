b8cef599a873d89a895ac0add48ff60c012b7f5e
Vatsal Patel <vatsal4011@gmail.com>
2023-05-21 11:33:58 +0530

    why does build keep failing

--- pages/sign-in/[[...index]].js
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-const SignInPage = () => (
-  <div className="flex flex-row items-center justify-center my-20">
-    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-  </div>
-);
-
-export default SignInPage;
